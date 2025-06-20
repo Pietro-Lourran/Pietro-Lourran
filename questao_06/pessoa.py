3768bd3d5e8bddd8fb61e7a7aacca279ef832b3b
Pietro-Lourran <139568816+Pietro-Lourran@users.noreply.github.com>
2025-06-20 12:12:26 -0300

    Delete questao_06/pessoa.py

--- questao_06/pessoa.py
+++
@@ -1,6 +0,0 @@
-''' Armazena dados de alunos e professores e calcula as medias dos alunos'''
-# Classe básica que representa uma pessoa
-class Pessoa:
-    def __init__(self, nome, cpf):
-        self.nome = nome
-        self.cpf = cpf
