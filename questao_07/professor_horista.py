148ffad58669b2c07f7bbaf362cc378d77f5556b
Pietro-Lourran <139568816+Pietro-Lourran@users.noreply.github.com>
2025-06-20 12:10:13 -0300

    Create professor_horista.py

---
+++ questao_07/professor_horista.py
@@ -0,0 +1,5 @@
+# Professor Horista herda de Professor e calcula o salário 
+class ProfessorHorista(Professor):
+    def __init__(self, nome, cpf, horas, valor_hora):
+        salario = horas * valor_hora
+        super().__init__(nome, cpf, salario)
