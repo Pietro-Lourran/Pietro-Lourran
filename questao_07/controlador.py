69e4b26c94d168bc62befe8e41db17208b9f5a9e
Pietro-Lourran <139568816+Pietro-Lourran@users.noreply.github.com>
2025-06-20 12:11:01 -0300

    Create controlador.py

---
+++ questao_07/controlador.py
@@ -0,0 +1,23 @@
+# Controla listas de alunos e professores
+class Controlador:
+    def __init__(self):
+        self.alunos = []
+        self.professores = []
+
+    def adicionar_aluno(self, aluno):
+        self.alunos.append(aluno)
+
+    def adicionar_professor(self, professor):
+        self.professores.append(professor)
+
+    def buscar_aluno_por_nome(self, nome):
+        for aluno in self.alunos:
+            if aluno.nome == nome:
+                return aluno
+        return None
+
+    def buscar_professor_por_nome(self, nome):
+        for professor in self.professores:
+            if professor.nome == nome:
+                return professor
+        return None
