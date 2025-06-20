22239b8ced7e7b40951136efa47bcf0abfa59804
Pietro-Lourran <139568816+Pietro-Lourran@users.noreply.github.com>
2025-06-20 11:58:51 -0300

    Create teste5.py

---
+++ questao_05/teste5.py
@@ -0,0 +1,25 @@
+if __name__ == "__main__":
+    # Escolha os pontos 
+    p1 = Ponto(1, 3)
+    p2 = Ponto(2, 1)
+
+    # Imprime os pontos
+    print("P1 =", p1)          
+    print("P2 =", p2)           
+
+    # Soma dos pontos 
+    soma = p1 + p2
+    print("P1 + P2 =", soma)   
+
+    # Subtração dos pontos 
+    sub = p1 - p2
+    print("P1 - P2 =", sub)     
+
+    # Produto escalar 
+    produto = p1 * p2
+    print("P1 * P2 =", produto) 
+
+    # Escalar * ponto 
+    escalar = 3
+    multiplicado = escalar * p1
+    print(f"{escalar} * P1 =", multiplicado)  
