cb8eec9994084107d5189c6162b42304a0e77624
Pietro-Lourran <139568816+Pietro-Lourran@users.noreply.github.com>
2025-06-20 12:08:41 -0300

    Create aluno.py

---
+++ questao_07/aluno.py
@@ -0,0 +1,10 @@
+# Matrícula e notas
+class Aluno(Pessoa):
+    def __init__(self, nome, cpf, matricula, notas):
+        super().__init__(nome, cpf)  # chamada da classe Pessoa
+        self.matricula = matricula
+        self.notas = notas
+
+    def mostrar_media(self):
+        media = self.notas.calcular_media()
+        print(f"Média de {self.nome}: {media:.2f}")
