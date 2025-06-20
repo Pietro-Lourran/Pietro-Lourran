8f61b96f1f85d63d7596d7b44ea2671d221a0c86
Pietro-Lourran <139568816+Pietro-Lourran@users.noreply.github.com>
2025-06-20 12:05:46 -0300

    Create pessoa.py

---
+++ questao_07/pessoa.py
@@ -0,0 +1,6 @@
+''' Armazena dados de alunos e professores e calcula as medias dos alunos'''
+# Classe básica que representa uma pessoa
+class Pessoa:
+    def __init__(self, nome, cpf):
+        self.nome = nome
+        self.cpf = cpf
