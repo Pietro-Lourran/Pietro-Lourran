e600fa30565e5811d55b6d3ffcb3a742ce4d0142
Pietro-Lourran <139568816+Pietro-Lourran@users.noreply.github.com>
2025-06-20 11:19:57 -0300

    Create teste.py

---
+++ questao_01/teste.py
@@ -0,0 +1,20 @@
+ # teste 
+def main():
+    print("Criando o Veículo")
+    meu_carro = Veiculo(economia=12, limiteTanque=45)  
+
+    meu_carro.getCombustivel()      
+    meu_carro.dirigir(50)             
+
+    meu_carro.encher(20)              
+    meu_carro.dirigir(100)            
+
+    meu_carro.getCombustivel()       
+    meu_carro.encher(30)              
+
+    meu_carro.dirigir(500)            
+    meu_carro.getCombustivel()       
+
+# Verifica se  está sendo executado diretamente 
+if __name__ == "__main__":
+    main()
